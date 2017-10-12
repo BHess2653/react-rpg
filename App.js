a66dfcf82fbe628ab7920ba056d4c0fe6ac398de
BHess2653 <GM_Dahaka@BHMac.local>
2017-10-12 18:50:09 -0400

    updating app.js changing P1

--- App.js
+++ App.js
@@ -43,7 +43,12 @@ class Player1 extends React.Component {
     const { params } = this.props.navigation.state;
     return (
       <View>
-        <Text style={styles.text}>Inspecting {params.user}</Text>
+      <Text style={styles.text}>Stats:</Text>
+        <Text style={styles.text}>HP: 100/100</Text>
+        <Text style={styles.text}>XP: 1250/2000</Text>
+        <Text style={styles.text}>$: 10g 10s 10c</Text>
+        <Text style={styles.text}>Inventory:</Text>
+        <Text style={styles.text}>Staff of Healing</Text>
       </View>
     );
   }
