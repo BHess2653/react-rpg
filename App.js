801d14b67b4cab50e2d1a723ff08e05f7027b193
BHess2653 <GM_Dahaka@BHMac.local>
2017-10-16 22:44:56 -0400

    updated/changing app.js

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { StyleSheet, FlatList, Text, View, Image, scrollView, TextInput, Button, AppRegistry } from 'react-native';
-import { StackNavigator } from 'react-navigation';
+import { StyleSheet, FlatList, Text, View, Image, scrollView, TextInput, AppRegistry } from 'react-native';
+import { StackNavigator, TabNavigator } from 'react-navigation';
+import Button from 'apsl-react-native-button';
 
 class ProfileScreen extends React.Component {
   static navigationOptions = {
-    headerRight: <Button title="Logout" />,
     title: 'GameMaster',
   };
   render() {
@@ -13,17 +13,17 @@ class ProfileScreen extends React.Component {
       <View>
         <Image
           style={styles.image}
-          source={require('./src/img/icon.png')}
+          source={require('./src/img/icon3.png')}
         />
         <Text style={[styles.text, styles.title]}>Options:</Text>
         <Button
+          style={[styles.btns, style={backgroundColor: '#5cb75c'}]}
           onPress={() => navigate('New', { user: 'New Game' })}
-          title="New Games"
-        />
+        ><Text style={styles.btnTxt}>New Game</Text></Button>
         <Button
+          style={[styles.btns, style={backgroundColor: '#f1ad4f'}]}
           onPress={() => navigate('Old', { user: 'Current Games' })}
-          title="Current Games"
-        />
+        ><Text style={styles.btnTxt}>Created Games</Text></Button>
         <View style={styles.views}>
           <Text style={[styles.text, styles.title]}>Profile Info:</Text>
           <Text style={styles.text}>Games Finished: 5</Text>
@@ -31,6 +31,10 @@ class ProfileScreen extends React.Component {
           <Text style={styles.text}>Characters Killed: 1</Text>
           <Text style={styles.text}>Characters Finished: 1</Text>
         </View>
+        <Button
+          style={[styles.btns]}
+          onPress={() => navigate('Log')}
+        ><Text style={style={fontSize: 20}}>Logout</Text></Button>
       </View>
     );
   }
@@ -48,6 +52,7 @@ class HomeScreen extends React.Component {
       <View>
         <Text style={styles.text}>Inspect Players</Text>
         <Button
+          style={[styles.btns]}
           onPress={() => navigate('P1', { user: 'theclaw124' })}
           title="theclaw124"
         />
@@ -56,7 +61,73 @@ class HomeScreen extends React.Component {
   }
 }
 
+class Login extends React.Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: 'Login',
+  });
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <View style={styles.currentGames}>
+        <TextInput style={styles.textInput} placeholder={"Username"}/>
+        <TextInput style={styles.textInput} placeholder={"Password"}/>
+        <Button
+          style={[styles.btns, style={backgroundColor: '#007aff'}]}
+          onPress={() => navigate('Profile')}
+        ><Text style={styles.btnTxt}>Login</Text></Button>
+      </View>
+    );
+  }
+}
+
+
 class NewGame extends React.Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: 'New Game',
+  });
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <View style={styles.currentGames}>
+        <TextInput style={styles.textInput} placeholder={"Campaign Name?"}/>
+        <TextInput style={styles.textInput} placeholder={"Terrain Type?"}/>
+        <TextInput style={styles.textInput} placeholder={"Starting Location?"}/>
+        <TextInput style={styles.textInput} placeholder={"Starting Enemies?"}/>
+        <TextInput style={styles.textInput} placeholder={"How Many Players?"}/>
+        <Button
+          style={[styles.btns, style={backgroundColor: '#007aff'}]}
+          onPress={() => navigate('Players', { user: 'New Game' })}
+        ><Text style={styles.btnTxt}>Create Game</Text></Button>
+      </View>
+    );
+  }
+}
+
+class Players extends React.Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: 'Players',
+  });
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <View style={styles.currentGames}>
+        <Button
+          style={[styles.btns, style={backgroundColor: '#007aff'}]}
+          onPress={() => navigate('Profile')}>
+          <Text style={styles.btnTxt}>Add Players</Text>
+        </Button>
+        <Text style={styles.text}>Send this code to your friends</Text>
+        <Text style={styles.text}>Code: cQ4Bx8</Text>
+        <Button
+          style={[styles.btns, style={backgroundColor: '#5cb75c'}]}
+          onPress={() => navigate('Profile')}
+        ><Text style={styles.btnTxt}>Done</Text></Button>
+      </View>
+    );
+  }
+}
+
+class NewGameold extends React.Component {
   static navigationOptions = ({ navigation }) => ({
     title: `${navigation.state.params.user}`,
   });
@@ -84,22 +155,23 @@ class OldGame extends React.Component {
   render() {
     const { params } = this.props.navigation.state;
     return (
-      <View>
+      <View style={styles.currentGames}>
         <TextInput style={styles.textInput} placeholder={"Game Name?"}/>
         <TextInput style={styles.textInput} placeholder={"How Many Players?"}/>
         <TextInput style={styles.textInput} placeholder={"Starting Location?"}/>
-        <Button
-          title="Create Game"
-        />
+        <TextInput style={styles.textInput} placeholder={"Starting Quest?"}/>
+        <Button style={[styles.btns]} ><Text style={style={fontSize: 20}}>Create Game</Text></Button>
       </View>
     );
   }
 }
 
 export const SimpleApp = StackNavigator({
+  Log: { screen: Login },
   Profile: { screen: ProfileScreen },
   New: { screen: NewGame },
   Old: { screen: OldGame },
+  Players: { screen: Players },
   Home: { screen: HomeScreen },
 });
 
@@ -116,6 +188,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center'
   },
+  currentGames: {
+    paddingTop: 200,
+  },
   text: {
     fontFamily: 'Avenir',
     fontWeight: '500',
@@ -127,7 +202,12 @@ const styles = StyleSheet.create({
     fontWeight: '500',
     textAlign: 'center',
     fontSize: 25,
-    height: 40,
+    width: 350,
+    height: 50,
+    marginTop: 5,
+    marginLeft: 30,
+    borderWidth: 2,
+    borderColor: '#bfbfbf',
   },
   title: {
     color: 'green',
@@ -135,12 +215,24 @@ const styles = StyleSheet.create({
   image: {
     width: 150,
     height: 150,
-    marginTop: 50,
-    marginBottom: 50,
+    marginTop: 30,
+    marginBottom: 30,
     marginLeft: 130,
     resizeMode: 'contain',
   },
   views: {
     marginTop: 20,
-  }
+  },
+  btns: {
+    width: 350,
+    height: 50,
+    marginTop: 5,
+    marginLeft: 30,
+    backgroundColor: 'lightgrey',
+    borderColor: 'rgba(241, 173, 79, 0)',
+  },
+  btnTxt: {
+    color: 'white',
+    fontSize: 20,
+  },
 });
